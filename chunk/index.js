0045e7347002f137d932b640b227a2d2fc73b0a8
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-04-18 15:53:23 +0200

    Solution 1

--- chunk/index.js
+++ chunk/index.js
@@ -9,29 +9,42 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+  //Solution 1
+  const chucked = [];
 
-  //My Solution (wrong)
-  //finding out how many chunks we need
-  var chunkCount = parseInt(Math.ceil(array.length / size));
+  for (const element of array) {
+    const last =  chucked[chucked.length - 1];
 
-  //Array where the data is stored
-  let arr = [chunkCount][size];
-
-  //Counter variable
-  let count = 0;
-
-  //filling the values into the array
-  for (let x = 0; x < chunkCount; x++) {
-    for (let y = 0; y < size; y++) {
-      arr[x][y] = array[count];
-
-      count++;
-    }    
+    if (!last || last.length === size) {
+      chucked.push([element]);
+    }else{
+      last.push(element);
+    }
   }
 
-  return arr;
-
+  return chucked;
 
 }
 
 module.exports = chunk;
+
+  // //My Solution (wrong)
+  // //finding out how many chunks we need
+  // var chunkCount = parseInt(Math.ceil(array.length / size));
+
+  // //Array where the data is stored
+  // let arr = [chunkCount][size];
+
+  // //Counter variable
+  // let count = 0;
+
+  // //filling the values into the array
+  // for (let x = 0; x < chunkCount; x++) {
+  //   for (let y = 0; y < size; y++) {
+  //     arr[x][y] = array[count];
+
+  //     count++;
+  //   }    
+  // }
+
+  // return arr;
