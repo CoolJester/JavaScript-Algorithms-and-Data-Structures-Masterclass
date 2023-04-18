38edff2a8de63315dd181eaf4c35068365b37270
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-04-18 11:42:15 +0200

    fizzbuzz exercise

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -12,6 +12,47 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  //Solution
+  for (let x = 1; x <= n; x++) {
+    //Checking the values we got
+    if (x % 3 === 0 && x %  5 === 0) {
+      //check the number if its a multiple of 3 and 5
+      console.log("fizzbuzz");
+    }else if(x % 3 === 0){
+      //Is the number a multiple of 3
+      console.log("fizz");
+    }else if(x % 5 === 0){
+      //Is the number a multiple of 3
+      console.log("buzz");
+    }else{
+      //When all the previous values aint multiples of 3 and 5
+      console.log(x);
+    }
+  }
+
+  
+}
 
 module.exports = fizzBuzz;
+
+// //My Solution (wrong)
+//   //loop
+//   for (let z = 1; z <= n; z++) {
+//     //skipping 1 and 2
+//     if (z == 1 || z == 2) {
+//       console.log(z);
+//       continue;
+//     }
+
+//     //checking if its a multiple of 3
+//     if(n % 3 == 0 && n % 5 == 0){
+//       console.log("fizzbuzz")
+//     }else if (n % 3 == 0) {
+//       console.log("fizz");
+//     } else if (n % 5 == 0) {
+//       console.log("buzz");
+//     }else{
+//       console.log(z);
+//     }
+//   }
