89cc6c244790613e661fa03bea2d2e20c9f5064f
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-04-19 01:35:20 +0200

    MY solution for anagrams

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,54 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  //My Solution
+  //control
+  let match = true;
+
+  //store the strings and a lowercase format with no spaces and puncuations
+  let wordOne = stringA.replace(/[^\w]/g, "").toLowerCase();
+  let wordTwo = stringB.replace(/[^\w]/g, "").toLowerCase();
+
+  //objects
+  let objOne = {};
+  let objTwo = {};
+
+  //Making the strings into objects
+  for (const char of wordOne) {
+    if (!objOne[char]) {
+      objOne[char] = 1;
+    }else{
+      objOne[char]++;
+    }
+  }
+  for (const char of wordTwo) {
+    if (!objTwo[char]) {
+      objTwo[char] = 1;
+    }else{
+      objTwo[char]++;
+    }
+  }
+
+  //loop through the onjects to see the matches
+  for (const key in objOne) {
+    if (objOne[key] == objTwo[key]) {
+      match = true;
+    }else{
+      match = false;
+    }
+  }
+
+  for (const key in objTwo) {
+    if (objTwo[key] == objOne[key]) {
+      match = true;
+    }else{
+      match = false;
+    }
+    
+  }
+
+  return match;
+}
 
 module.exports = anagrams;
