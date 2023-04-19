f929b329c9e98d88dbf3f3e6c73254e8b366ace5
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-04-19 03:46:33 +0200

    Solution 2 - Recursion

--- steps/index.js
+++ steps/index.js
@@ -17,22 +17,40 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  //Solution 1
-  for (let x = 0; x < n; x++) {
-    let stair = "";
-    for (let y = 0; y < n; y++) {
-      if (y <= x) {
-        stair += '#';
-      }else{
-        stair += ' ';
-      }
-    }
-    console.log(stair);
+//Solution 2 - using recursion
+function steps(n, row = 0, stair = '') {
+  //base case
+  if (n === row) {
+    return;    
   }
 
+  //checking the string
+  if (n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1)
+    return;
+  }
 
-  
+  if (stair.length <= row) {
+    stair += '#';    
+  }else{
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// //Solution 1
+// for (let x = 0; x < n; x++) {
+//   let stair = "";
+//   for (let y = 0; y < n; y++) {
+//     if (y <= x) {
+//       stair += '#';
+//     }else{
+//       stair += ' ';
+//     }
+//   }
+//   console.log(stair);
+// }
