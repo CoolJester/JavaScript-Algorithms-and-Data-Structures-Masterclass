eecbd29b3656e4e316493b84a768e56e32c65377
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-04-19 02:26:21 +0200

    My Solution

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  //My solution - trying to use the every function
+  let word = str.split('');
+
+  //first letter
+  word[0] = word[0].toUpperCase();
+
+  for (let x = 0; x < word.length; x++) {
+    if(word[x] === " "){
+      word[x + 1] = word [x + 1].toUpperCase();
+    }
+  }
+
+  return word.join('');
+}
 
 module.exports = capitalize;
