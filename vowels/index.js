bd50994136ae93186c7a5ae72b93c9f64a4e3a21
Ntsako Baloyi <ST10127198@rcconnect.edu.za>
2023-04-19 11:29:26 +0200

    My Solution

--- vowels/index.js
+++ vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //My Solution 
+  let counter = 0;
+  //check the vowels using a loop
+  for (const ele of str.toLowerCase()) {
+    //check the item
+    if(ele === 'a' || ele === 'e' || ele === 'i' || ele === 'o' || ele === 'u'){
+      counter++;
+    }
+  }
+
+  return counter;
+}
 
 module.exports = vowels;
